9ad6cd05809cb2d0db2e69f04dada2937a7eb9e5
Pat <hoang.patrick@outlook.com>
2024-06-05 09:12:15 +0200

    modification models

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import OuvrageController from "./controller/OuvrageController.js";
 import UserController from "./controller/UserController.js";
 import ReservationController from "./controller/ReservationController.js";
