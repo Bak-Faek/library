d45bf34a73fed57239664a60cd5e346a600e5c15
Pat <hoang.patrick@outlook.com>
2024-06-05 16:17:57 +0200

    ajout dossier front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Ouvrages from "./components/Ouvrages/Ouvrages";
+
+function App() {
+  return (
+    <div>
+      <Ouvrages />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Ouvrages/Ouvrages.jsx
@@ -0,0 +1,3 @@
+export default function Ouvrages() {
+    return <div>test</div>
+}
